04803d8f2f35efed5d4178019976b94e37d43d9e
cexperto <sipuedeshazloya@gmail.com>
2021-12-06 20:56:15 -0500

    settings for sqllite

--- campaña/settings.py
+++ campaña/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -175,3 +175,5 @@ REST_FRAMEWORK = {
     ],
     
 }
+
+django_heroku.settings(locals())
