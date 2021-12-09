1087d5a47ca2721b9ac2b5f30ef1692157d9f1cb
cexperto <sipuedeshazloya@gmail.com>
2021-12-09 10:25:28 -0500

    debug

--- campania/settings.py
+++ campania/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c7)6uzmw^s_bhq&mmy9fmcmp=0#hdt*_)c2$$ce6=xqge0n8w6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
